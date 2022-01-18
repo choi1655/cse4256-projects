591ee1a71ae2f8b277bb5f85ddf9b354c2b43e1c
John Choi <johnchoi1003@icloud.com>
2022-01-18 02:29:39 -0500

    Problem 1 done

---
+++ Homework-2/solution.py
@@ -0,0 +1,18 @@
+
+# CSE4256 Homework 2.
+# Author: John Choi choi.1655@osu.edu
+# Version: Jan 18, 2022
+
+# 1. s = "PYTHON"
+#   a) s[-1], s[5]
+#   b) 'PYTHON'
+#   c) 'PYT'
+#   d) 'PTO'
+#   e) s[:4]
+#   f) s[3::2]
+#   g) s[5::-2]
+
+
+
+if __name__ == "__main__":
+    print('hi')
