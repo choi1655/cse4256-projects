fff2f709b4fe0559b638249dbb43202b7e93c73d
John Choi <johnchoi1003@icloud.com>
2022-02-28 17:47:28 -0500

    triangles() and ctriangles() implemented

--- Homework-7/main.py
+++ Homework-7/main.py
@@ -4,19 +4,27 @@ Version: February 28, 2022
 
 The Ohio State University CSE4256 SP22 Homework 7.
 """
+import unittest
 
 # Problem 1
-def triangles(n):
+def triangles(n) -> list:
     """Write a function called triangles(n) that, given a positive integer n, produces
     a list of the first n triangle numbers. You may assume that n is a positive integer.
     """
-    pass
+    numbers = []
+    number = 0
+    for i in range(1, n + 1):
+        numbers.append(number + i)
+        number += i
+    return numbers
 
 def ctriangles(n):
     """Write a function called ctriangles(n) that produces a list identical to triangles(n), but does so using a
     single-line list comprehension. Hint: the built-in function sum(s) is probably useful here.
     """
-    pass
+    # 1, 3, 6, 10, 15
+    # 1, 2, 3, 4, 5
+    return [sum([j for j in range(i + 1)]) for i in range(1, n + 1)]
 
 def pascal(r):
     """Write a function called pascal(r) that, given a positive integer r, produces a list containing
@@ -25,3 +33,39 @@ def pascal(r):
     import any modules to complete this task, including the math module.
     """
     pass
+
+class TestTriangles(unittest.TestCase):
+
+    def test_triangles(self):
+        input = 5
+        expected = [1, 3, 6, 10, 15]
+        self.assertEqual(triangles(input), expected)
+
+    def test_triangles_0(self):
+        input = 0
+        expected = []
+        self.assertEqual(triangles(input), expected)
+
+    def test_triangles_1(self):
+        input = 1
+        expected = [1]
+        self.assertEqual(ctriangles(input), expected)
+
+    def test_ctriangles(self):
+        input = 5
+        expected = [1, 3, 6, 10, 15]
+        self.assertEqual(ctriangles(input), expected)
+
+    def test_ctriangles_0(self):
+        input = 0
+        expected = []
+        self.assertEqual(ctriangles(input), expected)
+
+    def test_ctriangles_1(self):
+        input = 1
+        expected = [1]
+        self.assertEqual(ctriangles(input), expected)
+
+# run the test cases
+if __name__ == '__main__':
+    unittest.main()
