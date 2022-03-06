2dea2ba2dd81cacc6a08fc7b4c37f317dba27ba9
johnchoi96 <johnchoi1003@icloud.com>
2022-03-06 14:14:20 -0500

    removing TODO

--- Homework-7/words.py
+++ Homework-7/words.py
@@ -12,7 +12,7 @@ def words(s):
 	    yield word
 
 def firstlines(filename):
-    """TODO Use the generator function words(s) to write a function called firstlines(filename)
+    """Use the generator function words(s) to write a function called firstlines(filename)
     that builds a dictionary of every word that appears in a file mapped to the line number on which it first appears.
     For example, suppose the contents of the file foo.txt are
     this is
