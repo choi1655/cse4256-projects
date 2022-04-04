eb3c24596f78be2fce89745f245bae09e0183de9
John Choi <johnchoi1003@icloud.com>
2022-04-03 22:22:57 -0400

    Finalizing

--- Homework-9/main.py
+++ Homework-9/main.py
@@ -7,7 +7,7 @@ The Ohio State University CSE4256 SP22 Homework 9.
 
 import cards
 
-# TODO: This main program should raise no exceptions when run with "python3 main.py".
+# This main program should raise no exceptions when run with "python3 main.py".
 # Implement the functions in cards.py and change the csv filename and n_players argument as needed,
 # but do not otherwise change this code.
 the_deck = cards.std_card_deck()
